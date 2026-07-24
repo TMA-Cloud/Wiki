426feb6c75d2d81fb31be488589b4969c33eefa2
Zinadin Zidan <zidan.roking@gmail.com>
2026-07-25 02:34:26 +0600

    Fix search 404 on GitHub Pages
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>

--- components/search.tsx
+++ components/search.tsx
@@ -27,6 +27,7 @@ export default function DefaultSearchDialog(props: SharedProps) {
   const { locale } = useI18n(); // (optional) for i18n
   const { search, setSearch, query } = useDocsSearch({
     client: oramaStaticClient({
+      from: `${process.env.NEXT_PUBLIC_BASE_PATH ?? ''}/api/search`,
       initOrama,
       locale,
     }),
